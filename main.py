324efb56274b75503ab55c0890c11cc9788e4287
Vetreria <90621103+Vetreria@users.noreply.github.com>
2022-05-10 01:19:59 +0300

    setup DialogFlow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from cgitb import text
 import logging
+from turtle import update
 import dotenv
 import os
 from telegram import Update, ForceReply
@@ -8,12 +10,13 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Callb
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
-
 logger = logging.getLogger(__name__)
+
+dotenv.load_dotenv()
+project_id = os.getenv('GOOGLE_CLOUD_PROJECT_ID')
 
 
 def start(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /start is issued."""
     user = update.effective_user
     update.message.reply_markdown_v2(
         fr'Здравствуйте {user.mention_markdown_v2()}\!',
@@ -22,23 +25,31 @@ def start(update: Update, context: CallbackContext) -> None:
 
 
 def help_command(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /help is issued."""
     update.message.reply_text('Help!')
 
 
-def echo(update: Update, context: CallbackContext) -> None:
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
+def detect_intent_texts(update: Update, context: CallbackContext, language_code = 'ru-RU'):
+    from google.cloud import dialogflow
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, update.message.chat_id)
+    text_input = dialogflow.TextInput(text=update.message.text, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+    response = session_client.detect_intent(
+    request={"session": session, "query_input": query_input}
+        )
+    update.message.reply_text("Fulfillment text: {}\n".format(response.query_result.fulfillment_text))
 
 
 def main() -> None:
     dotenv.load_dotenv()
     tg_token = os.environ["TG_TOKEN"]
+    os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
+    os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
     updater = Updater(tg_token)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, detect_intent_texts))
     updater.start_polling()
     updater.idle()
 
